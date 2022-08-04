4881bb76edfb29573b6193f25ae1f989d949dcd5
Dudsdudy <tiago-de-santana@hotmail.com>
2022-08-03 22:44:19 -0300

    Resolução Par ou Impar

--- Aula2/jogo.js
+++ Aula2/jogo.js
@@ -6,29 +6,61 @@
 //Math.random() ele sorteia de forma aleatoria o numero que quero
 //ParseInt() transforma o numero em inteiro
 
-let numeroSorteado = parseInt(Math.random() * 10 ) + 1
 
-console.log(numeroSorteado)
+//vars globais de controle || parametrização do jogo
+const PAR = "par"
+const IMPAR = "impar"
+let numeroSorteado = 0
+let ehPar = false
+let contVitoriasJogador = 0
+let contVitoriasCpu = 0
 
-let lado = prompt("Escolha seu lado: \n Par ou Impar")
 
-while(lado != PAR && lado !== IMPAR){
-    alert("Escolha seu lado: \n Par ou Impar")
-    console.log("digite novamente")
+console.log(
+    "numeroSorteado", numeroSorteado,
+    "\nehPar", ehPar
+)
 
+//O jogo
+while([vai ter uma condição]) {
+    sorteiaNumero()
+    descobreVencedor()
 }
 
 
-if (lado === PAR){
-    console.log("Escolheu par")
-}
-else if (lado === IMPAR){
-    console.log("Escolheu impar")
-}
 
-else {
-    console.log("digite novamente")
+
+
+//functions
+function sorteiaNumero() {
+    numeroSorteado = parseInt(Math.random() * 10) + 1
 }
 
-console.log(lado)
+function descobreVencedor() {
+    // operador ternário: [pergunta] ? [true] : [false]
+    ehPar = numeroSorteado % 2 === 0 ? true : false
 
+    let ladoJogador = lerEvalidarEntrada()
+
+    if ((ladoJogador === PAR && ehPar) ||
+        (ladoJogador === IMPAR && !ehPar)) {
+        console.log("Parabéns Jogador!!! Uhhhhu")
+        contVitoriasJogador++
+    } else {
+        console.log("iiiiiiiiii 😭")
+        contVitoriasCpu += 1
+    }
+}
+
+function lerEvalidarEntrada() {
+    let input = prompt("Escolha seu lado: \n Par ou Impar")
+
+    while (input !== PAR && input !== IMPAR) {
+        alert("digite novamente")
+        input = prompt("Escolha seu lado: \n Par ou Impar")
+    }
+
+    return input
+}
+
+console.log("fim")
